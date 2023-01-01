6564441d113a3e1753d7f61320c8bd7a13be5bdc
L <111347225+LLawliet0904@users.noreply.github.com>
2023-01-01 11:32:35 +0530

    Add files via upload

---
+++ area finder.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{ float length;
+printf("Enter your length: ");
+scanf ("%f" , &length);
+float breadth;
+printf("Enter your breadth: ");
+scanf ("%f", &breadth);
+printf("area is :%f",length * breadth);
+    return 0;
+}
